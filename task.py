2c8f9af6956b4836347d835407046601ad1f488a
Carinthorn <tthunyaphon2547@gmail.com>
2023-09-28 04:51:26 +0700

    add Planner

--- task.py
+++ task.py
@@ -1,2 +1,26 @@
 import tkinter as tk
- 
+from tkinter import *
+import calendar
+
+root = tk.Tk()
+class Planner():
+    def __init__(self): 
+        menubar = Menu(root)
+        day = Menu(menubar, tearoff = 0)
+        week = Menu(menubar, tearoff = 0)
+        month = Menu(menubar, tearoff = 0)
+        root.config(menu = menubar)
+        
+Planner()
+root.mainloop()
+        
+        
+        
+# class Day(): 
+    
+
+# class Week():
+    
+    
+# class Month():
+    
