e9da656f86e17bb34111363b2741c5c29f219692
Carinthorn <tthunyaphon2547@gmail.com>
2023-09-28 05:08:55 +0700

    add function

--- task.py
+++ task.py
@@ -1,2 +1,11 @@
 import tkinter as tk
- 
+class Planner():
+    def __init__(self): 
+        menubar = Menu(root)
+        day = Menu(menubar, tearoff = 0)
+        week = Menu(menubar, tearoff = 0)
+        month = Menu(menubar, tearoff = 0)
+        root.config(menu = menubar)
+        
+Planner()
+root.mainloop()
