e6bc29b4826d2aaba601a2d252e725a308f1f308
Carinthorn <tthunyaphon2547@gmail.com>
2023-10-04 22:48:50 +0700

    day done input2

--- task.py
+++ task.py
@@ -2,22 +2,81 @@ import tkinter as tk
 from tkinter import *
 import calendar 
 
-root = tk.Tk()
-class Planner():
-    def __init__(self): 
-        menubar = Menu(root)
-        day = Menu(menubar, tearoff = 0)
-        week = Menu(menubar, tearoff = 0)
-        month = Menu(menubar, tearoff = 0)
-        menubar.add_cascade(label = "Day", menu = day)
-        menubar.add_cascade(label = "Week", menu = week)
-        menubar.add_cascade(label = "Month", menu = month)
-        root.config(menu = menubar)
+#refactor frame and create a class for month Month(tk.Frame) + def(__inti__): tk.Frame parent somthing 
+window = tk.Tk()
+window.title("Task Manager")
+window.geometry("785x500")
+class Planner(tk.Tk):
+    def __init__(self, *arg, **kwargs):    
+        #problem
+        # menubar = Menu(root)
+        # day = Menu(menubar, tearoff = 0)
+        # week = Menu(menubar, tearoff = 0)
+        # month = Menu(menubar, tearoff = 0)
+        # menubar.add_cascade(label = "Day", menu = day)
+        # menubar.add_cascade(label = "Week", menu = week)
+        # menubar.add_cascade(label = "Month", menu = month)
+        # root.config(menu = menubar)
         
-Planner()
-root.mainloop()
+        #temporary menubar
+        # container = tk.Frame(self)
+        # container.pack(side="top", fill="both", expand=True)
+        # container.grid_rowconfigure(0, weight=1)
+        # container.grid_columnconfigure(0, weight=1)
+        menu =  Frame(window)
+        menu.grid(row = 0, column = 3, rowspan = 1, columnspan = 7, sticky= tk.W+tk.E)
+        Button(menu,text = "+", width = 3, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 0)
+        Button(menu,text = "Day", width = 3, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 1)
+        Button(menu,text = "Week", width = 3, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 2)
+        Button(menu,text = "Month", width = 3, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 3)
+        
+        change_bar = Frame(window)
+        change_bar.grid(row = 1, column = 6, rowspan = 1, columnspan = 1, sticky = tk.E)
+        Button(change_bar, text = "<", width = 1, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 5, sticky = "e")
+        Button(change_bar, text = "Today", width = 3, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 6, sticky = "e")
+        Button(change_bar, text = ">", width = 1, borderwidth = 0, highlightthickness = 0).grid(row = 1, column = 7, sticky = "e")
+        
+        #calendar
+        current_date = "August" + " 2023 " + "BCE"
+        Label(window, text = current_date, font=("Arial", 25, "bold")).grid(row = 2, column = 0, rowspan = 2, columnspan = 7, padx = 3, sticky = tk.W)
+        calendar_frame = Frame(window)
+        calendar_frame.grid(row = 4, column = 0, rowspan = 6, columnspan = 7, sticky= tk.W+tk.E )
+
+        #calendar day labels
+        day_labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        for i, day in enumerate(day_labels):
+            Label(calendar_frame, text = day, width = 7, height = 2, relief = "flat", borderwidth = 0, highlightthickness=0).grid(row = 3, column = i, sticky = tk.E)
+
+        row = 5
+        column = 0
+        days = calendar.monthcalendar(2023, 8)
+        for i in range(42): 
+            #fucking text problem
+            text=i+1
+            if text > 31:
+                text = (i - 31) + 1
+            if i == 11:
+                Button(calendar_frame,text=str(text) + "\n-Trip with homies\n-Mountain bike", width = 9, height = 3, relief = "flat", borderwidth = 0, highlightthickness=0, anchor="ne", bg= "#D9D9D9", command = self.display_info).grid(row = row, column = column)
+            else:
+                self.button = Button(calendar_frame, text=text, width = 9, height = 3, relief = "flat", borderwidth = 0, highlightthickness=0, anchor="ne", bg= "#A4DE9B", command = self.display_info).grid(row = row, column = column)
+            #button display info and add info
+            column += 1
+            if column == 7:
+                row += 1
+                column = 0
+        canvas = tk.Canvas(calendar_frame, width = 200, height = 100)
+        canvas.create_rectangle(50, 50, 200, 150, outline="black", fill="red")
 
         
+    def display_info(self, event):
+        #check database if the user added sth in a cell if not = add, if so = edit/delete
+        pass
+# class Month(): 
+if __name__ == "__main__": 
+    Planner()
+    window.mainloop()
+    
+
 # class Day(): 
     
 
---
+++ test.py
@@ -0,0 +1,106 @@
+# import pickle
+
+
+# data = {
+#     'Year': '2023',
+#     'Month': 'Oct',
+#     'Day' : [
+#         {
+#             'Date': '2',
+#             'Day_of_week' : 'Tues',
+#             'Time_slot' : [
+#                 {
+#                     '1' : {
+#                         'note' : 'sleep', 
+#                         'category' : 'none',
+#                         'location' : 'home',
+#                         'start' : '10.30AM',
+#                         'end' : '11.00AM',
+#                         'notify_me' : 'Yes'
+#                     }
+                    
+#                 }
+#             ]
+#         }
+#     ]
+# }
+
+# with open('data.pickle', 'wb') as file: 
+#     pickle.dump(data, file)
+    
+# loaded_data = None
+# with open('data.pickle', 'rb') as file: 
+#     loaded_data = pickle.load(file)
+
+# for i, n in enumerate(loaded_data['Day']):
+#     if n['Date'] == '2':
+#         n['Time_slot'].append({'2' : {
+#                         'note' : 'hangout', 
+#                         'category' : 'none',
+#                         'location' : 'mall',
+#                         'start' : '10.30AM',
+#                         'end' : '11.00AM',
+#                         'notify_me' : 'Yes'
+#                     }})
+#     elif i == len(loaded_data['Day']) - 1: 
+#         loaded_data['Day'].append({'Date': '2',
+#             'Day_of_week' : 'Tues',
+#             'Time_slot' : [
+#                 {
+#                     '1' : {
+#                         'note' : 'sleep', 
+#                         'category' : 'none',
+#                         'location' : 'home',
+#                         'start' : '10.30AM',
+#                         'end' : '11.00AM',
+#                         'notify_me' : 'Yes'
+#                     }
+                    
+#                 }
+#             ]})
+        
+# word = "10.30AM"
+# start = float(word[:len(word)-2])
+# print(start)
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+from calendar import month_name
+
+root = tk.Tk()
+
+# config the root window
+root.geometry('300x200')
+root.resizable(False, False)
+root.title('Combobox Widget')
+
+# label
+label = ttk.Label(text="Please select a month:")
+label.pack(fill=tk.X, padx=5, pady=5)
+
+# create a combobox
+selected_month = tk.StringVar()
+month_cb = ttk.Combobox(root, textvariable=selected_month)
+
+# get first 3 letters of every month name
+month_cb['values'] = [month_name[m][0:3] for m in range(1, 13)]
+
+# prevent typing a value
+month_cb['state'] = 'readonly'
+
+# place the widget
+month_cb.pack(fill=tk.X, padx=5, pady=5)
+
+
+# bind the selected value changes
+def month_changed(event):
+    """ handle the month changed event """
+    showinfo(
+        title='Result',
+        message=f'You selected {selected_month.get()}!'
+    )
+
+month_cb.bind('<<ComboboxSelected>>', month_changed)
+
+root.mainloop()
